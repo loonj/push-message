690fc7b2e116311228988bc8a91825c5144ddc88
loonj <loonj@163.com>
2020-03-15 16:02:50 +0800

    新增了一个Student类

--- app/src/main/java/com/afi/jintest01/Student.java
+++ app/src/main/java/com/afi/jintest01/Student.java
@@ -1,4 +1,22 @@
 package com.afi.jintest01;
 
 public class Student {
+    private int age;
+    private String name;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
