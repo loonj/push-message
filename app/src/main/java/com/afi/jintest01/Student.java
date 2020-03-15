0ba686691817c905f864f6e9f70a9b893ae7e86d
loonj <loonj@163.com>
2020-03-15 16:12:33 +0800

    dd

--- app/src/main/java/com/afi/jintest01/Student.java
+++ app/src/main/java/com/afi/jintest01/Student.java
@@ -3,6 +3,7 @@ package com.afi.jintest01;
 public class Student {
     private int age;
     private String name;
+    private int height;
 
     public int getAge() {
         return age;
@@ -18,5 +19,13 @@ public class Student {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
     }
 }
