2618d47f4a6d591740aa88a3f80797bf04b2259d
loonj <loonj@163.com>
2020-12-23 16:00:47 +0800

    随便测试一下

--- app/src/main/java/com/afi/jintest01/MainActivity.java
+++ app/src/main/java/com/afi/jintest01/MainActivity.java
@@ -20,7 +20,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.InstanceIdResult;
 import com.google.firebase.messaging.FirebaseMessaging;
-
+//2020年12月23日更新测试
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
